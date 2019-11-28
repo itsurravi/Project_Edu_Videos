9143dedb150bc258700ac00a39250c3135eaaa1e
ravi <ravik6373@gmail.com>
2019-11-28 10:06:20 +0530

    first

--- app/src/main/java/com/codrox/myapp/MainActivity.java
+++ app/src/main/java/com/codrox/myapp/MainActivity.java
@@ -1,14 +1,92 @@
 package com.codrox.myapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.transition.Slide;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.codrox.myapp.fragments.AccountFragment;
+import com.codrox.myapp.fragments.CartFragment;
+import com.codrox.myapp.fragments.HomeFragment;
+import com.codrox.myapp.fragments.LibraryFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private static final String BACK_STACK_ROOT_TAG = "root_fragment";
+
+    BottomNavigationView navigationItemView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationItemView = findViewById(R.id.navigation);
+        navigationItemView.setOnNavigationItemSelectedListener(this);
+
+        HomeFragment f = new HomeFragment();
+
+        FragmentManager manager = getSupportFragmentManager();
+//        manager.popBackStack(BACK_STACK_ROOT_TAG, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_container, f);
+        transaction.commit();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager manager = getSupportFragmentManager();
+
+        if (manager.getBackStackEntryCount() > 0) {
+            manager.popBackStack();
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                loadFragment(fragment,"Home");
+                break;
+            case R.id.nav_cart:
+                fragment = new CartFragment();
+                loadFragment(fragment,"Cart");
+                break;
+            case R.id.nav_lib:
+                fragment = new LibraryFragment();
+                loadFragment(fragment,"Library");
+                break;
+            case R.id.nav_account:
+                fragment = new AccountFragment();
+                loadFragment(fragment,"Account");
+                break;
+        }
+        return true;
+    }
+
+    private void loadFragment(Fragment fragment, String TAG) {
+        // load fragment
+        final FragmentManager manager = getSupportFragmentManager();
+//        manager.popBackStack(BACK_STACK_ROOT_TAG, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        transaction.replace(R.id.fragment_container, fragment, TAG);
+        transaction.addToBackStack(BACK_STACK_ROOT_TAG);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/codrox/myapp/fragments/LibraryFragment.java
@@ -0,0 +1,32 @@
+package com.codrox.myapp.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.codrox.myapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LibraryFragment extends Fragment {
+
+
+    public LibraryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_library, container, false);
+    }
+
+}
